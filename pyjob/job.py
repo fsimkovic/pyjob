53926ca0814dd55bf4017efe7b99ca5e8ae8f6ca
Felix Simkovic <felixsimkovic@me.com>
2017-06-06 12:38:48 +0100

    license added to every source file

--- pyjob/job.py
+++ pyjob/job.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2017 Felix Simkovic
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """Job container for automated launch"""
 
 __author__ = "Felix Simkovic"
@@ -138,7 +160,7 @@ class Job(object):
             self._log = [s.rsplit('.', 1)[0] + '.log' for s in script]
             self._script = list(script)
             # Prepare array scripts
-            if any(base.__name__ == "ClusterPlatform" for base in self._platform.__bases__):
+            if len(script) > 1 and any(base.__name__ == "ClusterPlatform" for base in self._platform.__bases__):
                 script, _ = prep_array_script(self._script, kwargs['directory'], self._platform.TASK_ENV)
                 kwargs["array"] = [1, len(self._script),
                                    kwargs['max_array_jobs'] if 'max_array_jobs' in kwargs else len(self._script)]
