223d53df38c967dbc29b08ec4caca9720b311328
Felix Simkovic <felixsimkovic@me.com>
2017-10-05 22:50:29 +0100

    patches

--- pyjob/version.py
+++ pyjob/version.py
@@ -2,7 +2,7 @@
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (0, 1, "4dev1")
+__version_info__ = (0, 1, "4b1")
 
 # ======================================================
 # Do __NOT__ change anything below here
