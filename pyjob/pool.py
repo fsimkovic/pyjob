940bc9a2750def403b7d941571738ccde3043163
Felix Simkovic <felixsimkovic@me.com>
2018-07-31 12:08:09 +0100

    docstring mod

--- pyjob/pool.py
+++ pyjob/pool.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 __author__ = 'Felix Simkovic'
+__date__ = '29 Jul 2018'
 __version__ = '1.0'
 
 from contextlib import contextmanager
@@ -32,8 +33,8 @@ def Pool(*args, **kwargs):
     """:obj:`~multiprocessing.pool.Pool` of processes to allow concurrent method calls
     
     This function creates a context for normal :obj:`~multiprocessing.pool.Pool` 
-    instances. This allows controlled termination for :obj:`RuntimeError` 
-    and :obj:`KeyboardInterrupt` exceptions.
+    instances. This allows controlled termination for :exc:`RuntimeError` 
+    and :exc:`KeyboardInterrupt` exceptions.
 
     Examples
     --------
@@ -41,6 +42,18 @@ def Pool(*args, **kwargs):
     >>> from pyjob import Pool
     >>> with Pool(processes=4) as pool:
     ...     pool.map(callable, [iterable])
+
+    Parameters
+    ----------
+    *args : tuple
+       Positional arguments for :obj:`~multiprocessing.pool.Pool`
+    **kwargs : dict
+       Keyword arguments for :obj:`~multiprocessing.pool.Pool`
+
+    Yields
+    ------
+    :obj:`~multiprocessing.pool.Pool`
+       An instance to a :obj:`~multiprocessing.pool.Pool`
 
     """
     pool = Pool(*args, **kwargs)
