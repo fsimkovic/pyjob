71cc4222d084c6d6c3dfc7d4a082a8de1c3e27d2
Felix Simkovic <felixsimkovic@me.com>
2018-07-31 20:31:08 +0100

    module for factory functions

---
+++ pyjob/factory.py
@@ -0,0 +1,42 @@
+import importlib
+import logging
+
+from pyjob.exception import PyJobUnknownTaskPlatform
+
+TASK_PLATFORMS = {
+    'local': ('pyjob.local', 'LocalTask'),
+    'lsf': ('pyjob.lsf', 'LoadSharingFacilityTask'),
+    'pbs': ('pyjob.pbs', 'PortableBatchSystemTask'),
+    'sge': ('pyjob.sge', 'SunGridEngineTask'),
+    'torque': ('pyjob.torque', 'TorqueTask'),
+}
+
+logger = logging.getLogger(__name__)
+
+
+def TaskFactory(platform, *args, **kwargs):
+    """Accessibility function for any :obj:`~pyjob.task.Task`
+    
+    Parameters
+    ----------
+    platform : str
+       The platform to create the :obj:`~pyjob.task.Task` on
+    *args : tuple
+       Any positional arguments relevant to the :obj:`~pyjob.task.Task`
+    **kwargs : dict
+       Any keyword arguments relevant to the :obj:`~pyjob.task.Task`
+
+    Raises
+    ------
+    :exc:`~pyjob.exception.PyJobUnknownTaskPlatform`
+       Unknown platform
+
+    """
+    platform = platform.lower()
+    if platform in TASK_PLATFORMS:
+        logger.debug('Found requested platform in available task list')
+        module, class_ = TASK_PLATFORMS[platform]
+        return getattr(importlib.import_module(module), class_)(*args,
+                                                                **kwargs)
+    else:
+        raise PyJobUnknownTaskPlatform('Unknown platform: %s' % platform)
