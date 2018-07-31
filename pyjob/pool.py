4fda7a77b29749f15f87023b02ab10695496e949
Felix Simkovic <felixsimkovic@me.com>
2018-07-31 12:10:05 +0100

    better docstring for link to external source

--- pyjob/pool.py
+++ pyjob/pool.py
@@ -6,8 +6,7 @@
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# copies of the Software, and to permit persons to whom the Software is # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
@@ -32,7 +31,7 @@ from multiprocessing import Pool
 def Pool(*args, **kwargs):
     """:obj:`~multiprocessing.pool.Pool` of processes to allow concurrent method calls
     
-    This function creates a context for normal :obj:`~multiprocessing.pool.Pool` 
+    This function creates a context for normal :obj:`multiprocessing.pool.Pool` 
     instances. This allows controlled termination for :exc:`RuntimeError` 
     and :exc:`KeyboardInterrupt` exceptions.
 
@@ -46,14 +45,14 @@ def Pool(*args, **kwargs):
     Parameters
     ----------
     *args : tuple
-       Positional arguments for :obj:`~multiprocessing.pool.Pool`
+       Positional arguments for :obj:`multiprocessing.pool.Pool`
     **kwargs : dict
-       Keyword arguments for :obj:`~multiprocessing.pool.Pool`
+       Keyword arguments for :obj:`multiprocessing.pool.Pool`
 
     Yields
     ------
     :obj:`~multiprocessing.pool.Pool`
-       An instance to a :obj:`~multiprocessing.pool.Pool`
+       An instance to a :obj:`multiprocessing.pool.Pool`
 
     """
     pool = Pool(*args, **kwargs)
