8e423188022efd797e68dfedf21d780844859c39
Felix Simkovic <felixsimkovic@me.com>
2018-08-11 19:10:36 +0100

    Python3.7 support; conftest.py fix for Python >3

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
 ]
 
 ENTRY_POINTS = {'console_scripts': ['pyjob = pyjob.__main__:main']}
