2b8f6273c01bf438adfef3ff2a4abe4eb2141673
Felix Simkovic <felixsimkovic@me.com>
2018-08-11 17:30:53 +0100

    modified helper functions a little

--- pyjob/tests/conftest.py
+++ pyjob/tests/conftest.py
@@ -2,27 +2,34 @@ __author__ = 'Felix Simkovic'
 
 import os
 import pytest
+pytest_plugins = ['helpers_namespace']
 
 from pyjob.script import Script
-
-pytest_plugins = ['helpers_namespace']
 
 TEMPLATE = """
 def fib(n):
     cache = [0, 1]
-    for i in range(n):
+    if n == 1:
+        return cache[0]
+    for i in range(n - 2):
         tmp = cache[0]
         cache[0] = cache[1]
         cache[1] += tmp
     return cache[1]
-n = {}; print('%dth fib is: %d' % (n, fib(n)))
 """
+
+
+@pytest.helpers.register
+def fibonacci(n):
+    exec (TEMPLATE)
+    return fib(n)
 
 
 @pytest.helpers.register
 def get_py_script(i, target):
     script = Script(shebang='#!/usr/bin/env python', prefix='pyjob', stem='test{}'.format(i), suffix='.py')
-    script.content.extend(TEMPLATE.format(target).split(os.linesep))
+    script.content.extend(TEMPLATE.split(os.linesep))
+    script.content.append("n = {}; print('%dth fib is: %d' % (n, fib(n)))".format(target))
     return script
 
 
