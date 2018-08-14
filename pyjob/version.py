df7ba8fcf5b562165ab2493e645a44ddd06b5016
Felix Simkovic <felixsimkovic@me.com>
2018-08-14 13:51:35 +0100

    changed dummy version to something more sensible

--- pyjob/version.py
+++ pyjob/version.py
@@ -25,7 +25,7 @@ __author__ = 'Felix Simkovic'
 
 # Maintain sematantic versioning. Further information can
 # be found here [http://semver.org/]
-__version_info__ = (1, 0, 0)
+__version_info__ = (0, 2, 0)
 
 # ======================================================
 # Do __NOT__ change anything below here
