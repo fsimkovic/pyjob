4f73c72d4ae390bfe917d568d439cbf06ca0d421
Felix Simkovic <felixsimkovic@me.com>
2018-08-16 11:15:08 +0100

    test cases tidied

--- pyjob/tests/test_local.py
+++ pyjob/tests/test_local.py
@@ -12,139 +12,108 @@ from pyjob.local import CPU_COUNT, LocalTask
 class TestLocalTaskTermination(object):
     def test_terminate_1(self):
         scripts = [pytest.helpers.get_py_script(i, 10000) for i in range(4)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        task = LocalTask(paths, processes=CPU_COUNT)
+        task = LocalTask(scripts, processes=CPU_COUNT)
         task.run()
         task.wait()
-        assert all(os.path.isfile(f) for f in logs)
-        pytest.helpers.unlink(paths + logs)
+        for f in task.log:
+            assert os.path.isfile(f)
+        pytest.helpers.unlink(task.script + task.log)
 
-    @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_terminate_2(self):
         scripts = [pytest.helpers.get_py_script(i, 10000) for i in range(4)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        task = LocalTask(paths, processes=CPU_COUNT)
+        task = LocalTask(scripts, processes=CPU_COUNT)
         task.run()
         task.close()
-        assert all(os.path.isfile(f) for f in logs)
-        pytest.helpers.unlink(paths + logs)
+        for f in task.log:
+            assert os.path.isfile(f)
+        pytest.helpers.unlink(task.script + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_terminate_3(self):
-        scripts = [pytest.helpers.get_py_script(i, 1000000) for i in range(10)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        task = LocalTask(paths, processes=min(CPU_COUNT, 2))
+        scripts = [pytest.helpers.get_py_script(i, 1000000) for i in range(4)]
+        task = LocalTask(scripts, processes=min(CPU_COUNT, 2))
         task.run()
-        assert not all(os.path.isfile(f) for f in logs)
+        assert not all(os.path.isfile(f) for f in task.log)
         task.close()
-        assert all(os.path.isfile(f) for f in logs)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(f) for f in task.log)
+        pytest.helpers.unlink(task.script + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_terminate_4(self):
         scripts = [pytest.helpers.get_py_script(i, 1000000) for i in range(5)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
+        files = []
 
         def innerf():
-            task = LocalTask(paths, processes=min(CPU_COUNT, 2))
+            task = LocalTask(scripts, processes=min(CPU_COUNT, 2))
             task.run()
+            files.extend(task.script + task.log)
 
         innerf()
-        pytest.helpers.unlink(paths + logs)
+        pytest.helpers.unlink(files)
 
     @pytest.mark.skip(reason='Unstable test')
     def test_terminate_5(self):
         scripts = [pytest.helpers.get_py_script(i, 1000000) for i in range(10)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=min(CPU_COUNT, 2)) as task:
+        with LocalTask(scripts, processes=min(CPU_COUNT, 2)) as task:
             with pytest.raises(PyJobError):
                 task.run()
-                pytest.helpers.unlink(paths[4:])
-        pytest.helpers.unlink(paths[:4] + logs)
+                pytest.helpers.unlink(task.script[4:])
+        pytest.helpers.unlink(task.script[:4] + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_terminate_6(self):
         scripts = [pytest.helpers.get_py_script(i, 10000) for i in range(100)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=CPU_COUNT) as task:
+        with LocalTask(scripts, processes=CPU_COUNT) as task:
             task.run()
             time.sleep(1)
             task.kill()
-        assert all(os.path.isfile(path) for path in paths)
-        assert any(os.path.isfile(log) for log in logs)
-        assert not all(os.path.isfile(log) for log in logs)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(path) for path in task.script)
+        assert any(os.path.isfile(log) for log in task.log)
+        assert not all(os.path.isfile(log) for log in task.log)
+        pytest.helpers.unlink(task.script + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_terminate_7(self):
         scripts = [pytest.helpers.get_py_script(i, 10000) for i in range(10000)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=min(CPU_COUNT, 2)) as task:
+        with LocalTask(scripts, processes=min(CPU_COUNT, 2)) as task:
             task.run()
             time.sleep(5)
             task.kill()
-        assert all(os.path.isfile(path) for path in paths)
-        assert any(os.path.isfile(log) for log in logs)
-        assert not all(os.path.isfile(log) for log in logs)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(path) for path in task.script)
+        assert any(os.path.isfile(log) for log in task.log)
+        assert not all(os.path.isfile(log) for log in task.log)
+        pytest.helpers.unlink(task.script + task.log)
 
 
 class TestLocalPerformance(object):
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_performance_1(self):
         scripts = [pytest.helpers.get_py_script(i, 1000) for i in range(4)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=CPU_COUNT) as task:
+        with LocalTask(scripts, processes=CPU_COUNT) as task:
             task.run()
-        for path, log in zip(paths, logs):
-            assert os.path.isfile(log)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(f) for f in task.log)
+        pytest.helpers.unlink(task.script + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_performance_2(self):
         scripts = [pytest.helpers.get_py_script(i, 1000) for i in range(16)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=CPU_COUNT) as task:
+        with LocalTask(scripts, processes=CPU_COUNT) as task:
             task.run()
-        assert all(os.path.isfile(f) for f in logs)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(f) for f in task.log)
+        pytest.helpers.unlink(task.script + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_performance_3(self):
         scripts = [pytest.helpers.get_py_script(i, 1000) for i in range(32)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=CPU_COUNT) as task:
+        with LocalTask(scripts, processes=CPU_COUNT) as task:
             task.run()
-        assert all(os.path.isfile(f) for f in logs)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(f) for f in task.log)
+        pytest.helpers.unlink(task.script + task.log)
 
     @pytest.mark.skipif(pytest.on_windows, reason='Deadlock on Windows')
     def test_performance_4(self):
         scripts = [pytest.helpers.get_py_script(i, 10000) for i in range(64)]
-        [s.write() for s in scripts]
-        paths = [s.path for s in scripts]
-        logs = [s.path.replace('.py', '.log') for s in scripts]
-        with LocalTask(paths, processes=CPU_COUNT) as task:
+        with LocalTask(scripts, processes=CPU_COUNT) as task:
             task.run()
-        assert all(os.path.isfile(f) for f in logs)
-        pytest.helpers.unlink(paths + logs)
+        assert all(os.path.isfile(f) for f in task.log)
+        pytest.helpers.unlink(task.script + task.log)
