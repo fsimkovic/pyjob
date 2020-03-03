ef66f8f15ae2642e9040c7070dce7f6c3b305ea0
Felix Simkovic <felixsimkovic@me.com>
2020-03-03 21:02:13 +0000

    remove python2.7 and 3.5 support

--- setup.py
+++ setup.py
@@ -55,10 +55,9 @@ CLASSIFIERS = [
     "Intended Audience :: Developers",
     "Intended Audience :: Science/Research",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
 ]
 
 ENTRY_POINTS = {'console_scripts': ['pyjob = pyjob.__main__:main']}
